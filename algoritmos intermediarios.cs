cd984b553577c2f9c67e4228bf41759417ad1c10
Maycon1989 <146032026+Maycon1989@users.noreply.github.com>
2023-09-25 18:45:00 -0300

    Add files via upload

---
+++ algoritmos intermediarios.cs
@@ -0,0 +1,14 @@
+﻿decimal salariobruto = 0;
+
+Console.WriteLine("Digite o salário bruto: ");
+salariobruto = Convert.ToInt32(Console.ReadLine());
+
+if (salariobruto <= 900)
+{
+    Console.WriteLine("Imposto de renda: " + "isento");
+    Console.WriteLine("Sindicato: ");
+
+}
+
+
+
